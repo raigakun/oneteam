c4a580df095d546653ccdfa5c12a670ae1beca8e
raigakun <masashiyoshida2017@gmail.com>
2020-04-01 16:44:23 +0900

    ウィザード形式　追加

---
+++ db/migrate/20200401062824_create_favorites.rb
@@ -0,0 +1,11 @@
+class CreateFavorites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :favorites do |t|
+      t.text :team
+      t.text :player
+      t.text :headcoach
+      t.references :user
+      t.timestamps
+    end
+  end
+end
