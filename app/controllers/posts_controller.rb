8850cbef35befcbb81f0a9609bc7b9a47cda9ff8
raigakun <masashiyoshida2017@gmail.com>
2020-04-02 20:27:57 +0900

    ビュー枚数　増加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :destroy]
 
   def index
-    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
+    @posts = Post.limit(30).includes(:photos, :user).order('created_at DESC')
   end
 
   def new
