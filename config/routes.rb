2d63c65c07558a7c7b45a413bff08da93d0fff30
raigakun <masashiyoshida2017@gmail.com>
2020-04-02 13:32:07 +0900

    投稿機能実装と一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
     get 'favorites', to: 'users/registrations#new_favorite'
     post 'favorites', to: 'users/registrations#create_favorite'
   end
-  root to: "pages#home"
+  root to: "posts#index"
 
   resources :favorites, only: [:edit, :update]
 
   resources :users, only: [:show]
+
+  resources :posts, only: [:index, :new, :create] do
+    resources :photos, only: [:create]
+  end
 
 end
