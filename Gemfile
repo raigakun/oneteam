cfbfd72507f8e501723d20427cfcde957ea90410
raigakun <masashiyoshida2017@gmail.com>
2020-04-03 17:22:29 +0900

    S3導入

--- Gemfile
+++ Gemfile
@@ -72,6 +72,7 @@ gem 'haml-rails'
 gem 'devise'
 gem 'pry-rails'
 gem 'carrierwave', '~> 2.0'
+gem 'fog-aws'
 gem "mini_magick"
 
 group :production do
--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,10 @@ set :repo_url,  'git@github.com:raigakun/oneteam.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{config/master.key}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.3' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.6.3' 
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
@@ -31,4 +32,23 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload master.key'
+    task :upload do
+      on roles(:app) do |host|
+        if test "[ ! -d #{shared_path}/config ]"
+          execute "mkdir -p #{shared_path}/config"
+        end
+        upload!('config/master.key', "#{shared_path}/config/master.key")
+      end
+    end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
